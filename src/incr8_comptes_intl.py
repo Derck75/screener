86e944e4a7342bd5668f9bc8467e38d6d7545681
Derck75 <rayaneabb@gmail.com>
2026-09-20 20:51:16 +0200

    Update incr8_comptes_intl.py

--- src/incr8_comptes_intl.py
+++ src/incr8_comptes_intl.py
@@ -40,7 +40,8 @@ PLACE = {".PA": "epa", ".AS": "ams", ".BR": "ebr", ".LS": "els", ".IR": "dub",
          ".DE": "etr", ".MI": "mil", ".MC": "bme", ".VI": "vie", ".AT": "ath",
          ".ST": "sto", ".CO": "cph", ".HE": "hel", ".OL": "osl", ".IC": "ice",
          ".WA": "war", ".PR": "pra", ".BD": "bud", ".L": "lon", ".SW": "swx",
-         ".T": "tyo"}
+         ".T": "tyo",
+         ".KS": "kos", ".TW": "twn", ".TO": "tor"}
 
 # Le compte de resultat rendait zero poste sur MC.PA comme sur ASML.AS, alors
 # que bilan et flux fonctionnaient. Plusieurs chemins sont donc essayes et le
